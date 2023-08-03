c37f6a22516c1ddf8cbced87aef9bb897959f7a2
Stephanie Hazlitt <stephhazlitt@gmail.com>
2023-08-03 14:01:17 -0700

    add tests

---
+++ tests/testthat/test-lib-summary.R
@@ -0,0 +1,15 @@
+test_that("lib_summary returns expected results", {
+  expect_silent(result <- lib_summary())
+  expect_equal(ncol(result), 2)
+  expect_s3_class(result, "data.frame")
+  expect_equal(names(result), c("library", "n_packages"))
+  expect_type(result$library, "character")
+  expect_type(result$n_packages, "integer")
+  expect_gt(sum(result$n_packages), 0)
+  expect_equal(sort(result$library), sort(.libPaths()))
+  expect_setequal(result$library, .libPaths())
+})
+
+test_that("lib_summary fails appropriatley", {
+  expect_error(lib_summary("foo"), "unused argument")
+})
