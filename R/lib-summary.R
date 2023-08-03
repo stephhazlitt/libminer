58271c568c41a67b9ac80d3481a6028ea8ee62a4
Stephanie Hazlitt <stephhazlitt@gmail.com>
2023-08-03 11:59:07 -0700

    doc lib_summary

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,14 @@
+#' R Libraries Summary
+#'
+#' Provides a brief summary of the locally-installed
+#' R libraries
+#'
+#' @return A data.frame containing a count of packages in
+#'         each of your libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_table <-  table(pkgs[, "LibPath"])
