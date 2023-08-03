203938388e03b0fe1fbe83632aac78c600c130f3
Stephanie Hazlitt <stephhazlitt@gmail.com>
2023-08-03 14:29:27 -0700

    add sizes arg; add arg tests

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -9,10 +9,20 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
   pkgs <- utils::installed.packages()
   pkg_table <-  table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_table, stringsAsFactors = FALSE)
   names(pkg_df) <- c("library", "n_packages")
+
+if (sizes) {
+  pkg_df$lib_size <- vapply(
+    pkg_df$library,
+    function(x){
+      sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+    },
+    FUN.VALUE = double(1)
+  )
+}
   pkg_df
 }
